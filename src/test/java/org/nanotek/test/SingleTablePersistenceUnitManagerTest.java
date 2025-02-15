a65ba7ef095021950cdc603d5ba7a1c163714ea2
Jose Carlos Canova <jose.c.canova@gmail.com>
2025-02-15 12:04:16 -0300

    added simple crud unit tests for persistence unit

--- src/test/java/org/nanotek/test/SingleTablePersistenceUnitManagerTest.java
+++ src/test/java/org/nanotek/test/SingleTablePersistenceUnitManagerTest.java
@@ -1,19 +1,25 @@
 package org.nanotek.test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import java.util.Set;
 
+import org.hibernate.Session;
+import org.instancio.Instancio;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.support.MergingPersistenceUnitManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Query;
 import jakarta.persistence.metamodel.Metamodel;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
 
 @SpringBootTest
 public class SingleTablePersistenceUnitManagerTest {
@@ -24,11 +30,17 @@ public class SingleTablePersistenceUnitManagerTest {
 	@Autowired 
 	EntityManagerFactory entityManagerFactory;
 	
+	@Autowired
+	InjectionClassLoader classLoader;
+	
+	@Autowired
+	PlatformTransactionManager defaultTransactionManager;
+	
 	public SingleTablePersistenceUnitManagerTest() {
 	}
 	
 	@Test
-	void testEntityManagerInitialization() {
+	void testEntityManagerInitialization() throws ClassNotFoundException {
 		assertNotNull(entityManager);
 		System.out.println(entityManager);
 		assertNotNull(entityManagerFactory);
@@ -41,6 +53,34 @@ public class SingleTablePersistenceUnitManagerTest {
 		Query query = rm.createQuery("select a from org.nanotek.data.SimpleTable a");
 		List<?> resultList = query.getResultList();
 		assertNotNull(resultList);
+		assertNotNull(classLoader);
+		executeInsert(rm);
+		Query query1 = rm.createQuery("select a from org.nanotek.data.SimpleTable a");
+		List<?> resultList1 = query1.getResultList();
+		assertTrue(resultList1.size()==1);
+		executeDelete(rm);
+		Query query11 = rm.createQuery("select a from org.nanotek.data.SimpleTable a");
+		List<?> resultList11 = query11.getResultList();
+		assertTrue(resultList11.size()==0);
+	}
+
+	private void executeDelete(EntityManager rm) {
+        EntityTransaction tx = rm.getTransaction();
+        Query query11= rm.createQuery("delete from org.nanotek.data.SimpleTable a");
+        tx.begin();
+		query11.executeUpdate();
+        tx.commit();
+		
+	}
+
+	private void executeInsert(EntityManager rm) throws ClassNotFoundException {
+        EntityTransaction tx = rm.getTransaction();
+        tx.begin();
+		Class<?> clazz = Class.forName("org.nanotek.data.SimpleTable" , false , classLoader);
+		Object simpletable = Instancio.create(clazz);
+		rm.persist(simpletable);
+		rm.flush();
+		tx.commit();
 	}
 
 }
