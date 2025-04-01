39a626e3cc0164d0cb8498387f6d1a75ab258197
Jose Carlos Canova <jose.c.canova@gmail.com>
2025-04-01 14:31:11 -0300

    Created sample spring data rest with 2 entities
    using Person-Pet example and byte-buddy construction of entities and
    repository, it was successfully created the model and repositories.

---
+++ src/test/java/org/nanotek/test/dynamic/entity/BirectionalJpaDataServiceConfiguration.java
@@ -0,0 +1,101 @@
+package org.nanotek.test.dynamic.entity;
+
+import javax.sql.DataSource;
+
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.nanotek.repository.data.MetaClassJpaTransactionManager;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.jpa.support.MergingPersistenceUnitManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import com.zaxxer.hikari.HikariConfig;
+
+import jakarta.persistence.ValidationMode;
+
+
+@SpringBootConfiguration
+@EnableJpaRepositories(basePackages = "jimfs.org.nanotek.config.spring.repositories")
+@EnableAutoConfiguration(exclude= {TransactionAutoConfiguration.class})
+public class BirectionalJpaDataServiceConfiguration implements ApplicationContextAware{
+
+	public BirectionalJpaDataServiceConfiguration() {
+	}
+	
+	ApplicationContext context;
+	
+	@Bean
+	@Primary
+	@ConfigurationProperties(prefix = "spring.datasource")
+	public HikariConfig hikariConfig() {
+		return new HikariConfig();
+	}
+
+	@Bean(value="myPersistenceManager")
+	@Qualifier(value="myPersistenceManager")
+	@DependsOn("dataSource")
+	public MergingPersistenceUnitManager myPersistenceManager(@Autowired DataSource dataSource) {
+		MergingPersistenceUnitManager pum = new  MergingPersistenceUnitManager();
+		pum.setValidationMode(ValidationMode.NONE);
+		pum.setDefaultPersistenceUnitName("buddyPU");
+		pum.setPackagesToScan("org.nanotek.config.spring.data");
+		pum.setDefaultDataSource(dataSource);
+		return pum;
+	}
+	
+	@Bean(name = "entityManagerFactory")
+	@Qualifier(value="entityManagerFactory")
+	@DependsOn("myPersistenceManager")
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+			@Autowired DataSource dataSource ,
+			@Autowired @Qualifier("myPersistenceManager") MergingPersistenceUnitManager myPersistenceManager) {
+		
+		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+		factory.setDataSource(dataSource);
+		factory.setPersistenceUnitManager(myPersistenceManager);
+		factory.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+		factory.setJpaDialect(new HibernateJpaDialect());
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		factory.setJpaVendorAdapter(vendorAdapter);
+		return factory;
+	}
+	
+
+	@Primary
+	@Bean("transactionManager")
+	@Qualifier(value="transactionManager")
+	@DependsOn("entityManagerFactory")
+	public PlatformTransactionManager transactionManager(
+			@Autowired DataSource dataSource ,
+			@Autowired	@Qualifier("entityManagerFactory") LocalContainerEntityManagerFactoryBean factory) {
+			MetaClassJpaTransactionManager transactionManager = new MetaClassJpaTransactionManager();
+	      transactionManager.setEntityManagerFactory(factory.getObject());
+	      transactionManager.setJpaDialect(new HibernateJpaDialect());
+	      transactionManager.setJpaPropertyMap(factory.getObject().getProperties());
+	      transactionManager.setDataSource(dataSource);
+	      transactionManager.setNestedTransactionAllowed(true);
+//		return new HibernateTransactionManager(factory.unwrap(SessionFactory.class));
+		return transactionManager;//new DataSourceTransactionManager(dataSource);
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.context = applicationContext;
+	}
+	
+
+}
