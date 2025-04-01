2eca8fec090fcc5d8aa8ff35a0af2c92ed0af30b
Jose Carlos Canova <jose.c.canova@gmail.com>
2025-04-01 12:06:25 -0300

    Added tables to test dynamictype birectional relation definition.

---
+++ src/test/resources/unit_test_tables/person_pet_tables.sql
@@ -0,0 +1,18 @@
+
+CREATE TABLE public.person
+(
+   person_key varchar(50) PRIMARY KEY  NOT NULL,
+   person_name varchar(200) NOT NULL)
+;
+CREATE TABLE public.pet
+(
+   pet_key varchar(25) PRIMARY KEY  NOT NULL,
+   pet_name varchar(250) NOT NULL,
+   person_key varchar(50) NOT NULL
+)
+;
+ALTER TABLE public.pet
+ADD CONSTRAINT pet_fkey
+FOREIGN KEY (person_key)
+REFERENCES public.person(person_key)
+;
