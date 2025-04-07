a4fca31f6aa71ffb4d655312d67646e9a17bac7f
Jose Carlos Canova <jose.c.canova@gmail.com>
2025-04-07 08:45:34 -0300

    renamed table column to facilitate model definition debug

--- src/test/resources/unit_test_tables/person_pet_tables.sql
+++ src/test/resources/unit_test_tables/person_pet_tables.sql
@@ -8,11 +8,11 @@ CREATE TABLE public.pet
 (
    pet_key varchar(25) PRIMARY KEY  NOT NULL,
    pet_name varchar(250) NOT NULL,
-   person_key varchar(50) NOT NULL
+   pet_person_key varchar(50) NOT NULL
 )
 ;
 ALTER TABLE public.pet
 ADD CONSTRAINT pet_fkey
-FOREIGN KEY (person_key)
+FOREIGN KEY (pet_person_key)
 REFERENCES public.person(person_key)
 ;
