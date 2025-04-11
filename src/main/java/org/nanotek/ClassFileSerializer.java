c5be72c2a1f8ebed86ae46028cb434e218068543
Jose Carlos Canova <jose.c.canova@gmail.com>
2025-04-11 12:08:55 -0300

    Added file serialization gitignore problem

---
+++ src/main/java/org/nanotek/ClassFileSerializer.java
@@ -0,0 +1,43 @@
+package org.nanotek;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.FileAttribute;
+
+import org.nanotek.config.MetaClassVFSURLClassLoader;
+
+public interface ClassFileSerializer {
+
+public static void saveEntityFile(Class<?> c, MetaClassVFSURLClassLoader bytearrayclassloader2) {
+		
+		String directoryString = "/home/jose/Documents/";
+		
+		String fileName =  c.getName().replaceAll("[.]","/").concat(".class");
+		
+		InputStream is = bytearrayclassloader2.getResourceAsStream(fileName);
+
+		try 
+		{
+
+			byte[] classBytes = is.readAllBytes();
+			var className = c.getName();
+			var simpleName = c.getSimpleName();
+			Path dirPath = Paths.get(directoryString, new String[] {});
+			Files.createDirectories(dirPath);
+			var classLocation  = directoryString.concat("/").concat(simpleName).concat(".class");
+			Path classPath = Paths.get(classLocation, new String[] {});
+			if(!Files.exists(classPath, LinkOption.NOFOLLOW_LINKS))
+    			Files.createFile(classPath, new FileAttribute[0]);
+			Files.write(classPath, classBytes, StandardOpenOption.WRITE);
+
+			
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		
+	}
+}
