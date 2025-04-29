eb545d222c83046496a556b6e99bbe3dbba07894
Jose Carlos Canova <jose.c.canova@gmail.com>
2025-04-29 17:06:04 -0300

    removed unused classes

--- src/test/java/org/nanotek/test/jpa/repositories/PetRepository.java
+++
@@ -1,7 +0,0 @@
-package org.nanotek.test.jpa.repositories;
-
-import org.nanotek.test.jpa.data.Pet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PetRepository extends JpaRepository<Pet, String> {
-}
